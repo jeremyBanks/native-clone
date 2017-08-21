f5c75e3e34e223f146c762e42ae74013e8051edb
Jeremy Banks <_@jeremy.ca>
2017-08-21 16:08:13 -0400

    adds untested IndexedDB implementation

--- nativeClone.ts
+++ nativeClone.ts
@@ -13,7 +13,11 @@ export let cloneAsync = cloneAsyncFallback;
 export const canCloneAsync = () => cloneSync !== cloneSyncFallback || canCloneSync();
 
 const asyncImplementationFactories:(() => undefined|(<T>(x:T) => Promise<T>))[] = [
-  () => { // MessageChannel implementation
+  (/*
+    MessageChannel implementation
+
+    No side-effects.
+  */) => {
     if (typeof MessageChannel !== 'function') return;
     if (typeof Map !== 'function') return;
 
@@ -41,14 +45,75 @@ const asyncImplementationFactories:(() => undefined|(<T>(x:T) => Promise<T>))[] 
       });
     };
   },
-  () => { // self.postMessage implementation
+
+  (/*
+    IndexedDB implementation
+
+    Side effect: creates an empty IndexedDB, but nothing should care.
+  */) => {
+    if (typeof indexedDB !== 'object' || !indexedDB) return;
+
+    const loadDb:Promise<IDBDatabase> = new Promise((resolve, reject) => {
+      const dbName = '/tmp/github.com/jeremybanks/native-clone';
+      const request = indexedDB.open(dbName, 1);
+      
+      request.onerror = event => reject(request.error);
+
+      request.onupgradeneeded = event => {
+        const db = request.result as IDBDatabase;
+
+        for (const name of Array.from(db.objectStoreNames)) {
+          db.deleteObjectStore(name);
+        }
+
+        const store = db.createObjectStore('pending');
+      }
+
+      request.onsuccess = event => {
+        const db = request.result as IDBDatabase;
+
+        resolve(db);
+      }
+    });
+    
+    const cloneAsync = <T>(value:T):Promise<T> => loadDb.then<T>(db => {
+      return new Promise((resolve, reject) => {
+        const transaction = db.transaction(['pending'], 'readwrite');
+        const objectStore = transaction.objectStore('pending');
+
+        const addRequest = objectStore.add(value, 1);
+        addRequest.onsuccess = event => {
+          const getRequest = objectStore.get(1);
+          getRequest.onsuccess = event => {
+            resolve(getRequest.result);
+
+            // now that we have the cloned value, abort the transaction so we don't touch the database.
+            transaction.abort();
+          };
+          getRequest.onerror = event => reject(getRequest.error);
+        }
+        addRequest.onerror = event => reject(addRequest.error);
+      });
+    });
+
+    var o = {a: [1, 2, null]}
+    cloneAsync(o).then(c => console.log(o, c, o !== c ? "good" : "bad"));
+
+    return cloneAsync;
+  },
+
+  (/*
+    self.postMessage implementation
+
+    Side effect: dispatches message events on window, and other code may react unexpectedly.
+  */) => {
     if (typeof self !== 'object' || !self) return;
     if (typeof self.addEventListener !== 'function') return;
     if (typeof self.postMessage !== 'function') return;
 
     const pendingClones: Map<string, (clonedValue: any) => void> = new Map;
 
-    let prefix = `cloner-${Math.random() * 2147483647 ^ +new Date()}`;
+    let nonce = `native-clone-${Math.random() * 2147483647 ^ +new Date()}`;
     let nextIndex: number = 0;
 
     self.addEventListener('message', event => {
@@ -68,7 +133,7 @@ const asyncImplementationFactories:(() => undefined|(<T>(x:T) => Promise<T>))[] 
 
     return <T>(value:T):Promise<T> => {
       return new Promise(resolve => {
-        const key = `${prefix}-${nextIndex++}`;
+        const key = `${nonce}-${nextIndex++}`;
         pendingClones.set(key, resolve);
         self.postMessage([key, value], '*');
       });
